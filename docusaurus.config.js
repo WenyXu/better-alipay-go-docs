99c0809a1f295a776cdc369d814446f74e38d073
WenyXu <wenymedia@gmail.com>
2021-02-16 21:48:39 +0900

    chroe(config.js): fix bugs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,7 +90,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/WenyXu/better-alipay-go-docs/edit/master/website/',
+            'https://github.com/WenyXu/better-alipay-go-docs/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
